0b8558a7ef42c50db7fd3137c376f861c76e0054
Dimitar Hristoskov <d_hristoskov@hotmail.com>
2020-06-29 15:38:30 +0200

    commit

--- client/src/CreateNewList/components/ProductItem.jsx
+++ client/src/CreateNewList/components/ProductItem.jsx
@@ -5,6 +5,7 @@ import { AiOutlineDelete,
     AiOutlineUndo,
     AiOutlineOrderedList } from 'react-icons/ai'
 import { GoIssueOpened, GoHistory } from 'react-icons/go';    
+import { FaMoneyBillWave } from 'react-icons/fa';
 
 import PriorityBtns from '../../shared-components/Buttons/PriorityBtns';
 
@@ -16,13 +17,14 @@ const PRIORITES = {
 
 const ProductItem = (props) => {
 
-    const [ icon, setIcon ] = useState(null)
-    const [ toggle, setToggle ] = useState(false)
-    //const taxes = 20;
+    const [ icon, setIcon ] = useState(null);
+    const [ toggle, setToggle ] = useState(false);
+    const [ toggledPrice, setToggledPrice ] = useState(false);
+    const taxes = 20;
 
     const price = parseFloat(props.price).toFixed(2);
     const totalPrice = parseFloat(price * props.qty).toFixed(2);
-    //const priceWithVat = parseFloat(props.price * (1 + taxes / 100)).toFixed(2);
+    const priceWithVat = parseFloat(props.price * (1 + taxes / 100)).toFixed(2);
 
     const togglePriority = () => {
         setToggle(prevState => !prevState)
@@ -30,13 +32,18 @@ const ProductItem = (props) => {
 
     const priorityHandler = (priority) => {
         setIcon(PRIORITES[priority])
-    }
+    };
+
+    const togglePrice = () => {
+        setToggledPrice(prevState => !prevState)
+    };
 
     return(
         <div className='product-wrapper' 
         style={props.isDone ? {background: '#D2DDBB', color: 'white'} : null}>
             <p className='name'>{props.name}</p>
-            <p className='price'>{price} €</p>
+            <p className='price'>{toggledPrice ?  priceWithVat : price } €
+            <span className='vat-icon' onClick={togglePrice}><FaMoneyBillWave/></span></p>
             <p className='quantity'>{props.qty}</p>
             <p className='type'>/ {props.type}</p>
             <p className='storage'>{props.storage}</p>
