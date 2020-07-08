0756f8e4466df0a0a5861cc9c4f0f8c9b9c381f8
Dimitar Hristoskov <d_hristoskov@hotmail.com>
2020-07-08 22:02:00 +0200

    commit

--- client/src/SavedProductLists/pages/SavedListsPage.jsx
+++ client/src/SavedProductLists/pages/SavedListsPage.jsx
@@ -1,12 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import axios from '../../axios';
 import { useParams } from 'react-router-dom';
 
 import SavedProductsList from '../components/SavedProductsList';
+import Loader from '../../shared-components/components/Loader/Loader';
 //import ProductStat from '../../shared-components/ProductStat/ProductStat';
 
 const StorageListsPage = () => {
   
+    const [ isLoading, setIsLoading ] = useState(false);
     const [ savedLists, setSavedLists ] = useState([])
     const { storageName } = useParams();
     
@@ -14,19 +16,29 @@ const StorageListsPage = () => {
     const fixedName = storageName.replace(/^./, str => str.toUpperCase());
 
     useEffect(() => {
+        setIsLoading(true);
         axios.get(`/archive/${storageName}`)
              .then(res => {
+                setIsLoading(false);
                 setSavedLists(res.data.lists)
              }).catch(err => {
-                 console.log(err)
+                setIsLoading(false);
+                console.log(err)
              })
     }, [storageName])
 
     return (
         <div className='main-wrapper'>
-            <p className='storage-name'>You are in <span>{fixedName}</span> storage.</p>
-            <SavedProductsList savedLists={savedLists}
-                             fixedName={fixedName}/>
+            {
+                isLoading 
+                ? <Loader />
+                :
+                <Fragment>
+                    <p className='storage-name'>You are in <span>{fixedName}</span> storage.</p>
+                    <SavedProductsList savedLists={savedLists}
+                                    fixedName={fixedName}/>
+                </Fragment>
+            }
             {/* <ProductStat /> */}
         </div>
     )
