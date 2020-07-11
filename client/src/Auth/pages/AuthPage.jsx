6879e020ce27bc74f617ba7ff485e3bfebc01a5a
Dimitar Hristoskov <d_hristoskov@hotmail.com>
2020-07-11 20:39:08 +0200

    commit

--- client/src/Auth/pages/AuthPage.jsx
+++ client/src/Auth/pages/AuthPage.jsx
@@ -1,48 +1,59 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
 import { useHistory } from 'react-router-dom';
 import axios from '../../axios';
 
 import CheckUser from '../components/CheckUser';
 import Registration from '../components/Registration';
 import Login from '../components/Login';
+import Loader from '../../shared-components/components/Loader/Loader';
 
 const AuthPage = () => {
 
-    const history = useHistory()
+    const history = useHistory();
+    const [ isLoading, setIsLoading ] = useState(false);
     const [ onLogin, setOnLogin ] = useState(null);
     const [ newEmail, setNewEmail ] = useState('');
 
     //Check if Email exist in DB
     const onAuthHandler = async (email) => {
+        setIsLoading(true);
         await axios.post('/users/check-email', email,
         { 'Content-Type': 'application/json' })
                    .then(res => {
+                    setIsLoading(false);
                     onLoginHandler(res.data.result)
                    }).catch(err => {
-                       console.log(err)
+                    setIsLoading(false);
+                    console.log(err)
                    });    
     };
 
     //Register new User
     const registerUser = async (user) => {
+        setIsLoading(true);
         await axios.post('/users/register', user,
         { 'Content-Type': 'application/json' })
                    .then(res => {
+                        setIsLoading(false);
                         console.log(res.data.token)
                         history.push('/storages')
                    }).catch(err => {
+                        setIsLoading(false);
                         console.log(err)
                    });
     };
 
     //Log-in existing User
     const loginUser = async (user) => {
+        setIsLoading(true);
         await axios.post('/users/login', user,
         { 'Content-Type': 'application/json' })
                    .then(res => {
+                        setIsLoading(false);
                         console.log(res.data.token)
                         history.push('/storages')
                    }).catch(err => {
+                        setIsLoading(false);
                         console.log(err)
                    });
     };
@@ -59,21 +70,27 @@ const AuthPage = () => {
 
 
     return (
-        <div>
+        <Fragment>
             {
-                onLogin === null ?
-                <CheckUser onAuthHandler={onAuthHandler} 
-                               setNewEmail={setNewEmail}/>
-                : null
+                isLoading 
+                ? <Loader />
+                : <div>
+                    {
+                        onLogin === null ?
+                        <CheckUser onAuthHandler={onAuthHandler} 
+                                    setNewEmail={setNewEmail}/>
+                        : null
+                    }
+                    {
+                        onLogin === 'login' ? <Login email={newEmail}
+                                                loginUser={loginUser}/> 
+                        : onLogin === 'register' ? <Registration email={newEmail} 
+                                                    registerUser={registerUser}/> 
+                        : null
+                    }         
+                 </div>
             }
-            {
-                onLogin === 'login' ? <Login email={newEmail}
-                                        loginUser={loginUser}/> 
-                : onLogin === 'register' ? <Registration email={newEmail} 
-                                            registerUser={registerUser}/> 
-                : null
-            }         
-        </div>
+        </Fragment>
     )
 }
 
